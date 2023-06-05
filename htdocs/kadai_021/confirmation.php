4cf4b6e52ace385a480087066ab34c52687917db
hirom319 <hmiu319@gmail.com>
2023-06-05 23:09:55 +0900

    php conf file template created

---
+++ htdocs/kadai_021/confirmation.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>21章課題</title>
+</head>
+
+<body>
+    <h2>入力内容をご確認ください。</h2>
+    <p>お名前：</p>
+    <p>性別</p>
+    <p>メールアドレス：</p>
+</body>
+
+
+</html>
