ef3e7130d8f229aa168325caf93ec521dd0db51d
hirom319 <hmiu319@gmail.com>
2023-06-05 23:02:55 +0900

    php form file updated

--- htdocs/kadai_021/form.php
+++ htdocs/kadai_021/form.php
@@ -16,8 +16,9 @@
             </tr>
             <tr>
                 <td>性別</td>
-                <td><input type="radio" name="gender" value="male">男性</td>
-                <td><input type="radio" name="gender" value="female">女性</td>
+                <td><input type="radio" name="gender" value="male">男性
+                    <input type="radio" name="gender" value="female">女性
+                </td>
             </tr>
             <tr>
                 <td>メールアドレス</td>
