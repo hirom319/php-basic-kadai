8c07e80677c429722bd64c3991428f93c146758c
hirom319 <hmiu319@gmail.com>
2023-06-05 22:50:01 +0900

    php form file updated

--- htdocs/kadai_021/form.php
+++ htdocs/kadai_021/form.php
@@ -7,7 +7,15 @@
 </head>
 
 <body>
-
+    <h2>個人情報入力フォーム</h2>
+    <form action="confirmation.php" method="post">
+        <label for="name">お名前</label>
+        <input type="text" id="name"><br>
+        <label for="gender">性別</label>
+        <input type="radio" id="gender"><br>
+        <label for="email">メールアドレス</label>
+        <input type="text" id="email"><br>
+    </form>
 </body>
 
 </html>
